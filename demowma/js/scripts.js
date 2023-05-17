6169b6ce4f521fc411cc4fc66dfe3f2bd907e9d7
TienLe166 <89723484+TienLe166@users.noreply.github.com>
2023-05-17 12:16:52 +0700

    Add files via upload

---
+++ demowma/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        window.location.href = "home.html"; // Đường dẫn đến trang index của bạn
+    }, 6000); // Thời gian chờ (tính bằng mili giây) trước khi chuyển hướng đến trang index
+});
